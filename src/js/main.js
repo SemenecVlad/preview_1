ff01019cfce5862867d35d30f0f9296b9a0b654a
Vladislav <vladmillionaire@gmail.com>
2016-11-03 11:06:14 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
+/**
+*@name Main application JS file
+*@description This is file contain all logic for web app
+*@author Vladislav Semenets <semenecvlad@mail.ru>
+*/
+
 //= controllers/LuxuryShopApp.js
 //= partials/_registerModule.js
